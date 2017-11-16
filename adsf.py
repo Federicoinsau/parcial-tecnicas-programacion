96942d5ca41d105d99a5cba03752a496ed5cf79b
Fede <federicoinsau@gmail.com>
2017-11-16 20:28:09 -0300

    ADSF es el nuevo main ejercicio2, pero no cambio nombres todavia porque el otro tiene la segunda parte

--- adsf.py
+++ adsf.py
@@ -1,23 +1,17 @@
 def reciboMapaYListaDeTuplas(mapa, listaDeTuplas):
-    listaDeBotes = []
+
     listaResultado = []
-    if mapa:
+
+    if mapa != [] and mapa != [""]:
         for fila in mapa:
-            for posicion in fila:
-                posicion = list(posicion)
-                if posicion == ["b"]:
-                    posicion = posicion.index("b")
-                    listaDeBotes.append(posicion)
-
-    for row in listaDeTuplas:
-        row = list(row)
-        row[0] = row[0] - 1
-        row[1] = row[1] - 1
-        listaResultado.append(row)
-
-
-
-
+            if not fila.isspace():
+                for posicion in fila:
+                    if posicion == "b" or ".":
+                        posicion = 0
+                    else:
+                        return listaResultado
+            else:
+                return listaResultado
     else:
         return listaResultado
 
@@ -30,11 +24,11 @@ def ejercicio2(mapa,listaDeTuplas):
 
 posicionesDeDisparosDePrueba = [(1,1),(3,4),(1,3),(4,5)]
 
-#assert (ejercicio2([],posicionesDeDisparosDePrueba) == [])
-#assert (ejercicio2([""],posicionesDeDisparosDePrueba) == [])
-#assert (ejercicio2(["      "],posicionesDeDisparosDePrueba) == [])
-#assert (ejercicio2(["soy NO valido"],posicionesDeDisparosDePrueba) == [])
+assert (ejercicio2([],posicionesDeDisparosDePrueba) == [])
+assert (ejercicio2([""],posicionesDeDisparosDePrueba) == [])
+assert (ejercicio2(["      "],posicionesDeDisparosDePrueba) == [])
+assert (ejercicio2(["soy NO valido"],posicionesDeDisparosDePrueba) == [])
 #assert (ejercicio2(["yo","tambien","soy","invalido"],posicionesDeDisparosDePrueba) == [])
 #assert (ejercicio2(["b.b.","....","..bb","b.b"],posicionesDeDisparosDePrueba) == [])
-assert (ejercicio2(["b.b..","b...b",".....","....b"],posicionesDeDisparosDePrueba) == [(2,1),(2,5)])
+#assert (ejercicio2(["b.b..","b...b",".....","....b"],posicionesDeDisparosDePrueba) == [(2,1),(2,5)])
 #assert (ejercicio2(["b..","...","..b"],[]) == [(1,1),(3,3)])
